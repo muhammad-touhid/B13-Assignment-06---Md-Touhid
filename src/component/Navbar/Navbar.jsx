d62eba81b6b4a0ff890355c1394652da25065082
muhammad-touhid <mdtouhidul59@gmail.com>
2026-05-27 13:43:27 +0600

    fixed navbar responsive

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = ({ cart }) => {
   return (
     <div className=" bg-base-100 shadow-sm py-4">
       <div className="navbar mx-auto max-w-300">
-        <div className="navbar-start">
+        <div className="navbar-start mt-8 sm:mt-0">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
               <svg
@@ -75,33 +75,37 @@ const Navbar = ({ cart }) => {
             </li>
           </ul>
         </div>
-        <div className="navbar-end space-x-4">
-          <div className="indicator">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-              />{" "}
-            </svg>
-            <span
-              className={`w-5 h-5 flex justify-center items-center pt-0.5 text-xs rounded-full indicator-item cursor-pointer ${cart.length > 0 ? "bg-red-500 text-white" : "bg-gray-300"}`}
-            >
-              {cart.length}
-            </span>
+        <div className="flex flex-col sm:flex-row gap-2 navbar-end justify-baseline space-x-4">
+          <div className="flex gap-5">
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />{" "}
+              </svg>
+              <span
+                className={`w-5 h-5 flex justify-center items-center pt-0.5 text-xs rounded-full indicator-item cursor-pointer ${cart.length > 0 ? "bg-red-500 text-white" : "bg-gray-300"}`}
+              >
+                {cart.length}
+              </span>
+            </div>
+            <p className="font-semibold cursor-pointer">Login</p>
           </div>
-          <p className="font-semibold cursor-pointer">Login</p>
-          <a className="btn rounded-4xl bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white">
-            Get Started
-          </a>
+          <div>
+            <a className="btn rounded-4xl bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white">
+              Get Started
+            </a>
+          </div>
         </div>
       </div>
     </div>
